896e37acc879fc6ef9f51052d39bda562dd99601
cary-rowen <manchen_0528@outlook.com>
2023-03-15 18:59:17 +0800

    Updated add-on template,make add-ons compatible with NVDA2023.1.

--- buildVars.py
+++ buildVars.py
@@ -23,24 +23,30 @@ addon_info = {
 	"addon_summary": _("以盲文表示法读出国际音标"),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
-	"addon_description": _("""本插件支持以盲文点位表示法读出国际音标，相关音标数据由浮云提供，
+	"addon_description": _("""本插件支持以盲文点位表示法读出国际音标。
 功能开关快捷键 NVDA+Shift+P。"""),
 	# version
-	"addon_version": "1.2",
+	"addon_version": "1.2.1",
 	# Author(s)
 	"addon_author": "Cary-rowen <manchen_0528@outlook.com>",
 	# URL for the add-on documentation support
-	"addon_url": "https://github.com/cary-rowen/TransIPA",
+	"addon_url": "https://www.nvdacn.com/",
+	# URL for the add-on repository where the source code can be found
+	"addon_sourceURL": "https://github.com/cary-rowen/TransIPA",
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": 2019.3,
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": 2022.1,
+	"addon_lastTestedNVDAVersion": 2023.1,
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
 	"addon_updateChannel": None,
+	# Add-on license such as GPL 2
+	"addon_license": None,
+	# URL for the license document the ad-on is licensed under
+	"addon_licenseURL": None,
 }
 
 # Define the python files that are the sources of your add-on.
